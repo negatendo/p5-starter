d447dfed810896e7fdaa11faea4258737681aa38
Brett O'Connor <brett@blimpsgo90.com>
2017-10-05 07:37:14 -0600

    better full-screen-ness

--- js/sketch.js
+++ js/sketch.js
@@ -1,25 +1,24 @@
-// some configurable stuff
-var canvasWidth = 1000;
-var canvasHeight = 700;
-var emojiImageDimension = 23;
-
 // for example content
+var emojiImageDimension = 23;
 var booped = false;
 
 function setup() {
-  // 1024x768 draw area
-  var canvas = createCanvas(1024, 768);
+  // fullscreen draw area
+  var canvas = createCanvas(windowWidth,windowHeight);
   // utilize html container for placement
   canvas.parent('container');
-  background('white');
+  background('black');
 }
+
+// use this to keep it fullscreen... er, fullbrowser
+function windowResized() { resizeCanvas(windowWidth, windowHeight); }
 
 function preload() {
   soundFormats('wav');
-
-  // load sample sounds
-  img = loadImage("template-assets/docomo-emoji/0157.png");
+  // load sample sound
   snd = loadSound("template-assets/nanoloop-samples/NL23_LR13.wav");
+  // load sample image
+  img = loadImage("template-assets/docomo-emoji/0157.png");
 
 }
 
@@ -33,8 +32,8 @@ function draw() {
 
 // example function that places image and plays sound
 function doBoop() {
-  var centerX = (canvasWidth/2) + (emojiImageDimension/2);
-  var centerY = (canvasHeight/2) + (emojiImageDimension/2);
+  var centerX = (windowWidth/2) + (emojiImageDimension/2);
+  var centerY = (windowHeight/2) + (emojiImageDimension/2);
   image(img, centerX, centerY);
   snd.setVolume(0.1);
   snd.play();
