e3e9d1313e026b3d7f49c5e2c9228960771b9e47
Brett O'Connor <brett@blimpsgo90.com>
2019-03-13 09:48:56 -0600

    smaller, faster, black background-er

--- js/sketch.js
+++ js/sketch.js
@@ -1,42 +1,18 @@
-// for example content
-var emojiImageDimension = 23;
-var booped = false;
-
 function setup() {
   // fullscreen draw area
   var canvas = createCanvas(windowWidth,windowHeight);
   // utilize html container for placement
   canvas.parent('container');
-  background('white');
+  background('black');
 }
 
 // use this to keep it fullscreen... er, fullbrowser
 function windowResized() { resizeCanvas(windowWidth, windowHeight); }
 
 function preload() {
-  soundFormats('wav');
-  // load sample sound
-  snd = loadSound("template-assets/nanoloop-samples/NL23_LR13.wav");
-  // load sample image
-  img = loadImage("template-assets/docomo-emoji/0157.png");
 
 }
 
 function draw() {
-  if (booped) {
-    return;
-  } else {
-    doBoop();
-  }
-}
 
-// example function that places image and plays sound
-function doBoop() {
-  var centerX = (windowWidth/2) + (emojiImageDimension/2);
-  var centerY = (windowHeight/2) + (emojiImageDimension/2);
-  image(img, centerX, centerY);
-  snd.setVolume(0.1);
-  snd.play();
-  booped = true;
 }
-
--- template-assets/nanoloop-samples/README.md
+++
@@ -1,3 +0,0 @@
-# Nanoloop Sample Pack
-
-These wav files are from the [Nanoloop Sample Pack](http://www.synthtopia.com/content/2011/07/03/nanoloop-2-3-sample-pack/) provided by Sebastian Tomczak.
